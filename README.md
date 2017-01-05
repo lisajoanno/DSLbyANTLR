8e9c4ebc1974292299ed7ea6eab19a664b79ea87
lisajoanno <lisa.joanno@gmail.com>
2017-01-05 09:16:21 +0100

    Refactor and ReadMe to use projet.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # DSLbyANTLR
 ANTLR implementation to recognize a DSL
+
+## How to compile
+
+**$ mvn package**
+
+- Compiles classes as usual
+- Also generates lexer, parser and listener interface in *src/main/java/grammarGenerated* package.
+
+> *Note* : the program uses lexer, parser and listener from the *src/main/java/grammar* package.
+
+> If the grammar in *src/main/antlr4/RuleSetGrammar.g4* was changed, one needs to manually copy the files of grammarGenerated in grammar.
+
+> Careful not to lose the content of the listener.
+
+
+**$ mvn exec:java**
+
+Execution set to recognize **input.txt** following the *src/main/antlr4/RuleSetGrammar.g4*.
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -16,21 +16,10 @@ import java.nio.file.Paths;
  *
  * Created by lisa.
  *
- * Pour utiliser :
- *
- * -    mvn generate-sources
- * -    copier le dossier (generated-sources/antlr4) grammars dans main ( . )
- *              (devrait générer un sous package main.grammars du coup)
- *              (c'est important que ce soit un sous package sinon même nom de package et problème de compil)
- * -    mvn package
- * -    mvn exec:java
- *
- * => ceci analyse le contenu de fileToParse.txt suivant la grammaire définie dans antlr4/RuleSetGrammar.g4
- *
  */
 public class Main {
     public static void main(String [] args) {
-        String fileName = "fileToParse.txt";
+        String fileName = "input.txt";
         try {
             String query = new String(Files.readAllBytes(Paths.get(fileName)));
             System.out.println("\n---------- Analyzing :\n" + query + "\n----------\n\n");
@@ -43,14 +32,6 @@ public class Main {
             RuleSetGrammarParser.DslContext tree = parser.dsl();
             RuleSetGrammarBaseListener listener = new RuleSetGrammarBaseListener();
             ParseTreeWalker.DEFAULT.walk(listener, tree);
-            //parser.addParseListener(new RuleSetGrammarBaseListener());
-
-            /*
-            ParseTree tree = parser.dsl(); // begin parsing at query rule
-            System.out.println("Number of syntax errors : " + parser.getNumberOfSyntaxErrors());
-            System.out.println("    --> toStringTree : ");
-            System.out.println(tree.toStringTree(parser)); // print LISP-style tree
-             */
         } catch (Exception e) {
             System.err.println("There was an exception somewhere");
         }
