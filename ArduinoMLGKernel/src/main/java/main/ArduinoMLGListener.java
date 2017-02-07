e6c0ecba6a7731b1b3addfd8599cd17bf82805e4
h4o <fvicente@polytech.unice.fr>
2017-02-07 18:18:44 +0100

    refactor of listener to take advantage of the listener pattern

--- ArduinoMLGKernel/src/main/java/main/ArduinoMLGListener.java
+++ ArduinoMLGKernel/src/main/java/main/ArduinoMLGListener.java
@@ -5,42 +5,110 @@ import grammar.RuleSetGrammarBaseListener;
 import grammar.RuleSetGrammarParser;
 import org.antlr.v4.runtime.misc.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by user on 25/01/17.
  */
 public class ArduinoMLGListener extends RuleSetGrammarBaseListener {
     private DSL dsl;
-    private String result;
+    private Transition transition;
+    private List<Condition> conditionList;
+    @Override
+    public void enterDebounce(@NotNull RuleSetGrammarParser.DebounceContext ctx){
+        dsl.setDebounce(Integer.parseInt(ctx.DIGIT().getText()));
+    }
+    @Override
+    public void enterSerial(@NotNull RuleSetGrammarParser.SerialContext ctx ){
+        dsl.setSerialActivated(BinaryState.valueOf(ctx.binaryState().getText().toString()) == BinaryState.HIGH);
+    }
+    @Override
+    public void enterSensor(@NotNull RuleSetGrammarParser.SensorContext ctx){
+        Sensor s = new Sensor();
+        s.setName(ctx.TEXT().toString());
+        s.setPin(Integer.parseInt(ctx.DIGIT().getText()));
+        dsl.addBrick(s);
+    }
+    @Override
+    public void enterActuator(@NotNull RuleSetGrammarParser.ActuatorContext ctx) {
+        Actuator a = new Actuator();
+        a.setName(ctx.TEXT().toString());
+        a.setPin(Integer.parseInt(ctx.DIGIT().getText()));
+        dsl.addBrick(a);
+    }
+
+    @Override
+    public void enterSpeaker(@NotNull RuleSetGrammarParser.SpeakerContext ctx){
+        Speaker s = new Speaker();
+        s.setName(ctx.TEXT().toString());
+        s.setPin(Integer.parseInt(ctx.DIGIT().getText()));
+        dsl.addBrick(s);
+    }
+
+    @Override
+    public void enterInit(@NotNull RuleSetGrammarParser.InitContext ctx){
+        dsl.setInitState(ctx.TEXT().toString());
+    }
+
+    @Override
+    public void enterLogicalAction(@NotNull RuleSetGrammarParser.LogicalActionContext ctx){
+        LogicalAction logicalAction = new LogicalAction();
+        logicalAction.setActuator((Actuator) dsl.getBrick(ctx.TEXT().toString()));
+        logicalAction.setBinaryState(BinaryState.valueOf(ctx.binaryState().getText().toString()));
+        RuleSetGrammarParser.StateContext stateContext = (RuleSetGrammarParser.StateContext)ctx.getParent().getParent();
+        State s = dsl.getState(stateContext.TEXT().toString());
+        s.addAction(logicalAction);
+    }
+    @Override
+    public void enterSerialPrint(@NotNull RuleSetGrammarParser.SerialPrintContext ctx){
+        RuleSetGrammarParser.StateContext stateContext = (RuleSetGrammarParser.StateContext)ctx.getParent().getParent();
+        State s = dsl.getState(stateContext.TEXT().toString());
+        SerialPrint serialPrint = new SerialPrint();
+        serialPrint.setText(ctx.TEXT().toString());
+        s.addAction(serialPrint);
+    }
+    @Override
+    public void enterTone(@NotNull RuleSetGrammarParser.ToneContext ctx){
+        Tone tone = new Tone();
+        tone.setFrequency(Long.parseLong(ctx.DIGIT(0).toString()));
+        tone.setLength(Long.parseLong(ctx.DIGIT(1).toString()));
+        tone.setSpeaker((Speaker)dsl.getBrick(ctx.TEXT().toString()));
+        RuleSetGrammarParser.StateContext stateContext = (RuleSetGrammarParser.StateContext)ctx.getParent().getParent();
+        State s = dsl.getState(stateContext.TEXT().toString());
+        s.addAction(tone);
+    }
+
+    @Override
+    public void enterTransition(@NotNull RuleSetGrammarParser.TransitionContext ctx ){
+        transition = new Transition();
+        transition.setTarget(dsl.getState(ctx.TEXT().toString()));
+        conditionList = new ArrayList<>();
+    }
+
+    @Override
+    public void enterLogicalCondition(@NotNull RuleSetGrammarParser.LogicalConditionContext ctx){
+        LogicalCondition logicalCondition = new LogicalCondition();
+        conditionList.add(logicalCondition);
+        logicalCondition.setSensor((Sensor) dsl.getBrick(ctx.TEXT().toString()));
+        logicalCondition.setBinaryState(BinaryState.valueOf(ctx.binaryState().toString()));
+    }
+    @Override
+    public void enterTimeCondition(@NotNull RuleSetGrammarParser.TimeConditionContext ctx){
+        TimeCondition timeCondition = new TimeCondition();
+        conditionList.add(timeCondition);
+        timeCondition.setWaitTime(Long.parseLong(ctx.DIGIT().toString()));
+    }
+
+    @Override
+    public void exitTransition(@NotNull RuleSetGrammarParser.TransitionContext ctx){
+        RuleSetGrammarParser.StateContext stateContext = (RuleSetGrammarParser.StateContext)ctx.getParent();
+        State s = dsl.getState(stateContext.TEXT().toString());
+        transition.setConditions(conditionList);
+        s.addTransition(transition);
+    }
     @Override public void enterDsl(@NotNull RuleSetGrammarParser.DslContext ctx) {
         dsl = new DSL();
-        boolean serialActivated = false;
-        if(ctx.serial() != null) {
-            serialActivated = BinaryState.valueOf(ctx.serial().binaryState().getText().toString()) == BinaryState.HIGH;
-        }
-        dsl.setSerialActivated(serialActivated);
-
-        if(ctx.debounce() != null){
-            dsl.setDebounce(Integer.parseInt(ctx.debounce().DIGIT().getText()));
-        }
-        for(RuleSetGrammarParser.SensorContext sc : ctx.sensor()){
-            Sensor s = new Sensor();
-            s.setName(sc.TEXT().toString());
-            s.setPin(Integer.parseInt(sc.DIGIT().getText()));
-            dsl.addBrick(s);
-        }
-
-        for(RuleSetGrammarParser.ActuatorContext ac : ctx.actuator()){
-            Actuator a = new Actuator();
-            a.setName(ac.TEXT().toString());
-            a.setPin(Integer.parseInt(ac.DIGIT().getText()));
-            dsl.addBrick(a);
-        }
-        for(RuleSetGrammarParser.SpeakerContext spc : ctx.speaker()){
-            Speaker s = new Speaker();
-            s.setName(spc.TEXT().toString());
-            s.setPin(Integer.parseInt(spc.DIGIT().getText()));
-            dsl.addBrick(s);
-        }
 
 
 
@@ -48,74 +116,12 @@ public class ArduinoMLGListener extends RuleSetGrammarBaseListener {
             State s = new State(sc.TEXT().toString());
             dsl.addState(s);
         }
-        for(RuleSetGrammarParser.StateContext sc : ctx.state()){
-            State s = dsl.getState(sc.TEXT().toString());
-            for(RuleSetGrammarParser.ActionContext ac : sc.action()){
-                Action action = null;
-                if(ac.logicalAction() != null) {
-                    LogicalAction logicalAction = new LogicalAction();
-                    RuleSetGrammarParser.LogicalActionContext lac = ac.logicalAction();
-                    logicalAction.setActuator((Actuator) dsl.getBrick(lac.TEXT().toString()));
-                    logicalAction.setBinaryState(BinaryState.valueOf(lac.binaryState().getText().toString()));
-                    action = logicalAction;
-                } else if (ac.serialPrint() != null){
-                    if(!serialActivated){
-                       // System.err.println("trying to print without serial, ignoring action !");
-                    }
-                    {
-                        SerialPrint serialPrint = new SerialPrint();
-                        RuleSetGrammarParser.SerialPrintContext spc = ac.serialPrint();
-                        serialPrint.setText(spc.TEXT().toString());
-                        action = serialPrint;
-                    }
-                } else {
-                    Tone tone = new Tone();
-                    RuleSetGrammarParser.ToneContext toneContext = ac.tone();
-                    tone.setFrequency(Long.parseLong(toneContext.DIGIT(0).toString()));
-                    tone.setLength(Long.parseLong(toneContext.DIGIT(1).toString()));
-                    tone.setSpeaker((Speaker)dsl.getBrick(toneContext.TEXT().toString()));
-                    action = tone;
-                }
-                if(action != null)
-                    s.addAction(action);
-            }
-
-            for(RuleSetGrammarParser.TransitionContext tc : sc.transition()){
-                Transition transition = new Transition();
-                transition.setTarget(dsl.getState(tc.TEXT().toString()));
-                for(RuleSetGrammarParser.ConditionContext cc : tc.condition()){
-                    Condition condition = null;
-                    if(cc.logicalCondition() != null){
-                        LogicalCondition logicalCondition = new LogicalCondition();
-                        RuleSetGrammarParser.LogicalConditionContext lc = cc.logicalCondition();
-                        logicalCondition.setBinaryState(BinaryState.valueOf(lc.TEXT().getText().toUpperCase()));
-                        logicalCondition.setSensor((Sensor)dsl.getBrick(lc.binaryState().getText().toString()));
-
-                        condition = logicalCondition;
-                    } else {
-                        RuleSetGrammarParser.TimeConditionContext tcc = cc.timeCondition();
-                        TimeCondition timeCondition = new TimeCondition();
-                        timeCondition.setWaitTime(Long.parseLong(tcc.DIGIT().getText()));
-                        condition = timeCondition;
-
-                    }
-                    transition.addCondition(condition);
-                }
-                s.addTransition(transition);
-            }
-
-        }
-
-        for(RuleSetGrammarParser.InitContext ic : ctx.init()){
-            dsl.setInitState(ic.TEXT().toString());
-        }
-        result = dsl.toString();
 
 
     }
 
 
     public String getProgram(){
-        return result;
+        return dsl.toString();
     }
 }
